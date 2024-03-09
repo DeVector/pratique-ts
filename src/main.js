c5a94309db17e078f5de22bfac481c5e77b0cbfc
DeVector <diniz.vito@gmail.com>
2024-03-08 21:23:33 -0300

    Commit initial

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicacao(5, 10));
+console.log(dizOla("Loren"));
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function dizOla(nome: string): string{
+    return `Olá ${nome}`;
+}
+
+
+console.log(multiplicacao(5, 10));
+
+console.log(dizOla("Loren"));
